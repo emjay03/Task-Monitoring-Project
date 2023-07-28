31af7987fe1c3350f41f04fc0bbf4e5e8e493670
MJ Cariso <111568194+emjay03@users.noreply.github.com>
2023-07-28 22:12:18 +0800

    backend clean

--- backend/connection.php
+++ backend/connection.php
@@ -47,124 +47,138 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["user_id"]) && isset($
         // Verify the password
         if ($password === $user["password"]) {
             // Password is correct
-            session_start();
+           
             $_SESSION['user_id'] = $user_id;
-    
-            // Check the user role
-            $role = $user["role"];
-            if ($role === "admin") {
-                // Redirect to the head admin dashboard
-                header("Location: ../frontend/Head/Headdashboard.php");
-                exit();
-            } else {
-                // Redirect to the normal user dashboard
-                header("Location: ../frontend/Team/Userdashboard.php");
-                exit();
-            }
+
+            // Return JSON response for successful login
+            echo json_encode(array("success" => true, "role" => $user["role"]));
+            exit();
         } else {
             // Invalid password
-            echo "Invalid password";
+            echo json_encode(array("success" => false, "message" => "Invalid password"));
+            exit();
         }
     } else {
         // User does not exist
-        echo "User not found";
+        echo json_encode(array("success" => false, "message" => "User does not exist"));
+        exit();
     }
 }
 
   
-
-$query = "SELECT * FROM usercredential Where role='Documentation' OR role='Frontend' OR role='Backend'OR role='UI/UX Designer'OR role='Database Designer'OR role='QA Tester'OR role='Content Creator'OR role='Business Analyst'";
+// Fetch all user data
+$query = "SELECT * FROM usercredential WHERE role IN ('Documentation', 'Frontend', 'Backend', 'UI/UX Designer', 'Database Designer', 'QA Tester', 'Content Creator', 'Business Analyst')";
 $stmt = $conn->prepare($query);
 $stmt->execute();
 $allUserData = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
  
-  //insert data to database 
- if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    if (isset($_POST['action'])) {
-        // Delete user by user_id
-        if ($_POST['action'] === 'delete' && isset($_POST['user_id'])) {
-            $user_id = $_POST['user_id'];
 
-            // Perform the delete query
-            $deleteQuery = "DELETE FROM usercredential WHERE user_id = :user_id";
-            $deleteStmt = $conn->prepare($deleteQuery);
-            $deleteStmt->bindParam(':user_id', $user_id);
-            $deleteStmt->execute();
+//Open Headteamtaskassign.php
+// Data insertion process
+if ($_SERVER["REQUEST_METHOD"] === "POST" && !isset($_POST['action'])) {
+    // Insert data to database
+    $user_id = $_POST['user_iduseracc'] ?? '';
+    $avatar_tmp_name = $_FILES['avataruseracc']['tmp_name'] ?? '';
+    $username = $_POST['usernameuseracc'] ?? '';
+    $password = $_POST['passworduseracc'] ?? '';
+    $role = $_POST['roleuseracc'] ?? '';
 
-            header("Location: ./HeadTeammember.php");
-            exit();
-        }elseif ($_POST['action'] === 'update' && isset($_POST['user_id'])) {
-            $user_id = $_POST['user_id'];
+    if (!empty($user_id)) {
+        // Hash the password before storing it in the database
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-            // Update data in the database
-            $username = $_POST['username'] ?? '';
-            $password = $_POST['passwordupdate'] ?? ''; // Update the name of the textarea input
-            $role = $_POST['roleupdate'] ?? ''; // Update the name of the textarea input
-
-            // Check if the user wants to update the avatar
-            $avatar = $_FILES['avatar']['tmp_name'] ?? '';
-            $imageData = null;
-            if (!empty($avatar)) {
-                // Read the binary image data from the uploaded file
-                $imageData = file_get_contents($avatar);
-            }
-
-            // Build the update query based on whether the user wants to update the avatar or not
-            if (empty($imageData)) {
-                $updateQuery = "UPDATE usercredential SET username = :username, password = :password, role = :role WHERE user_id = :user_id";
-            } else {
-                $updateQuery = "UPDATE usercredential SET username = :username, avatar = :avatar, password = :password, role = :role WHERE user_id = :user_id";
-            }
-
-            $updateuseracc = $conn->prepare($updateQuery);
-            $updateuseracc->bindParam(':user_id', $user_id);
-            $updateuseracc->bindParam(':username', $username);
-            if (!empty($imageData)) {
-                $updateuseracc->bindParam(':avatar', $imageData, PDO::PARAM_LOB); // Use PARAM_LOB to handle binary data
-            }
-            $updateuseracc->bindParam(':password', $password);
-            $updateuseracc->bindParam(':role', $role);
-            $updateuseracc->execute();
-
-            header("Location: ./HeadTeammember.php");
-            exit();
+        // Perform the file upload
+        if (!empty($avatar_tmp_name)) {
+            // Read the binary image data from the uploaded file
+            $imageData = file_get_contents($avatar_tmp_name);
+        } else {
+            // Set a default image data if no avatar is uploaded
+            $imageData = file_get_contents('default_avatar.jpg'); // Provide the path to your default avatar image
         }
-    } else {
-        // Insert data to database
-        $user_id = $_POST['user_id'] ?? ''; // Empty value
-        $avatar = $_FILES['avatar']['tmp_name'] ?? ''; // Empty value - get the temporary filename
-        $username = $_POST['username'] ?? ''; // Empty value
-        $password = $_POST['password'] ?? ''; // Empty value
-        $role = $_POST['role'] ?? ''; // Empty value
 
-        if (!empty($user_id)) {
-            // Perform the file upload
-            if (!empty($avatar)) {
-                // Read the binary image data from the uploaded file
-                $imageData = file_get_contents($avatar);
-            } else {
-                // Set a default image data if no avatar is uploaded
-                $imageData = file_get_contents('default_avatar.jpg'); // Provide the path to your default avatar image
-            }
+        // Insert data into the database
+        $insertQuery = "INSERT INTO usercredential (user_id, username, avatar, password, role) VALUES (:user_id, :username, :avatar, :password, :role)";
+        $insertStatement = $conn->prepare($insertQuery);
+        $insertStatement->bindParam(':user_id', $user_id);
+        $insertStatement->bindParam(':username', $username);
+        $insertStatement->bindParam(':avatar', $imageData, PDO::PARAM_LOB);
+        $insertStatement->bindParam(':password', $hashedPassword);
+        $insertStatement->bindParam(':role', $role);
+        $insertStatement->execute();
 
-            // Insert data into the database
-            $insertQuery = "INSERT INTO usercredential (user_id, username, avatar, password, role) VALUES (:user_id, :username, :avatar, :password, :role)";
-            $insertuseracc = $conn->prepare($insertQuery);
-            $insertuseracc->bindParam(':user_id', $user_id);
-            $insertuseracc->bindParam(':username', $username);
-            $insertuseracc->bindParam(':avatar', $imageData, PDO::PARAM_LOB); // Use PARAM_LOB to handle binary data
-            $insertuseracc->bindParam(':password', $password);
-            $insertuseracc->bindParam(':role', $role);
-            $insertuseracc->execute();
-
-            header("Location: ./HeadTeammember.php");
-            exit();
-        }
+        header("Location: ./HeadTeammember.php");
+        exit();
     }
 }
+ 
 
+//update user account for Headtaskassign.php
+
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['action']) && $_POST['action'] === 'update' && isset($_POST['user_id'])) {
+    // Update user data in the database
+    $user_id = $_POST['user_id'];
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['passwordupdate'] ?? '';
+    $role = $_POST['roleupdate'] ?? '';
+
+    // Check if the user wants to update the avatar
+    $avatar = $_FILES['avatar']['tmp_name'] ?? '';
+    $imageData = null;
+    if (!empty($avatar)) {
+        // Read the binary image data from the uploaded file
+        $imageData = file_get_contents($avatar);
+    }
+
+    // Build the update query based on whether the user wants to update the avatar or not
+    if (empty($imageData)) {
+        $updateQuery = "UPDATE usercredential SET username = :username, password = :password, role = :role WHERE user_id = :user_id";
+    } else {
+        $updateQuery = "UPDATE usercredential SET username = :username, avatar = :avatar, password = :password, role = :role WHERE user_id = :user_id";
+    }
+
+    $updateuseracc = $conn->prepare($updateQuery);
+    $updateuseracc->bindParam(':user_id', $user_id);
+    $updateuseracc->bindParam(':username', $username);
+    if (!empty($imageData)) {
+        $updateuseracc->bindParam(':avatar', $imageData, PDO::PARAM_LOB);
+    }
+    $updateuseracc->bindParam(':password', $password);
+    $updateuseracc->bindParam(':role', $role);
+    $updateuseracc->execute();
+
+    header("Location: ./HeadTeammember.php");
+    exit();
+}
+
+//delete user account for Headtaskassign.php
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['action']) && $_POST['action'] === 'delete' && isset($_POST['user_id'])) {
+ 
+    // Delete user by user_id
+    $user_id = $_POST['user_id'];
+    $deleteQuery = "DELETE FROM usercredential WHERE user_id = :user_id";
+    $deleteStmt = $conn->prepare($deleteQuery);
+    $deleteStmt->bindParam(':user_id', $user_id);
+    $deleteStmt->execute();
+
+    header("Location: ./HeadTeammember.php");
+    exit();
+}
+//Close Headteamtaskassign.php
+
+
+
+// if ($_SERVER["REQUEST_METHOD"] === "POST" && $_POST['action'] === 'delete' && isset($_POST['user_id'])) {
+//     // Delete user by user_id
+//     $user_id = $_POST['user_id'];
+//     $deleteQuery = "DELETE FROM usercredential WHERE user_id = :user_id";
+//     $deleteStmt = $conn->prepare($deleteQuery);
+//     $deleteStmt->bindParam(':user_id', $user_id);
+//     $deleteStmt->execute();
+
+//     header("Location: ./HeadTeammember.php");
+//     exit();
+// }
 $query = "SELECT * FROM usercredential WHERE user_id = :user_id";
 $stmt = $conn->prepare($query);
 $stmt->bindParam(':user_id', $user_id);
@@ -180,7 +194,7 @@ if ($result) {
 if ($result && !empty($result['avatar'])) {
     $imageData = $result['avatar'];
 }
-//update data to database
+// //update data to database
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Check if the required keys are present in $_POST array
     if (isset($_POST['updateusername'], $_POST['updatepassword'], $_POST['confirmpassword'])) {
@@ -349,3 +363,4 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
       $countStmt->execute();
       $totalRowCount = $countStmt->fetchColumn();
   }
+?>
